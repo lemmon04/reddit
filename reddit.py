8f8602636b67e47cc116a91c20e1937bef083092
Michael Lemmon <lemmon04@yahoo.com>
2017-08-07 11:31:19 -0700

    Create reddit.py

---
+++ reddit.py
@@ -0,0 +1,17 @@
+import praw
+
+#Fill in client_id & client_secret
+reddit = praw.Reddit(client_id="      ",
+                     client_secret='      ',
+                     user_agent='sumac')
+
+subreddit = reddit.subreddit('politics')
+for submission in subreddit.hot(limit=10):
+    print submission.url
+    print submission.id
+    
+submission = reddit.submission(url='https://www.reddit.com/r/politics/comments/6r5obm/rex_tillerson_rejects_80m-congress_fight-russian_propaganda-because_it_would_645554')
+for top_level_comment in submission.comments:
+    print(top_level_comment.body) + "\n"
+    
+
